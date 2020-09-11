36534c478dc04f89bfd8a76546aa2292f8c27a72
chic-state-0895 <brookeabryski@gmail.com>
2020-09-11 18:53:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,21 +1,39 @@
 class Song
-  @@count = 0
+  @@song_count = 0
   @@artist_count = 0
+  
   attr_accessor :name, :artist, :genre
 
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
-    @@count += 1
-    @@artist_count += 1
+    @@genres << @genre
+    @@artists << @artist
+    @@song_count += 1
   end
 
   def self.count
-    @@count
+    @@song_count
+  end
+  
+  def self.genres
+    @@genres.uniq 
+  end
+  
+  def self.artists
+    @@artist_uniq
   end
 
-  def self.artist_count
-    @@artist_count
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count [genre] genre_count[genre] +=1
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
   end
+  
 end
